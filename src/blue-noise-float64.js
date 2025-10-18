f96b45d45bda2d5acbdca476bbec44755908d236
🗣🗣:"ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-10-18 14:43:51 +0700

    Update blue-noise-float64.js

--- src/blue-noise-float64.js
+++ src/blue-noise-float64.js
@@ -1,13 +1,15 @@
 /**
- * Free JS implementation of Void and Cluster method by Robert Ulichney
+ * Free JS implementation of Void and Cluster method by Robert Ulichney and other methods
  * Ultra optimized while keeping it readable
  * The result is high quality blue noise but somehow very fast
- * 
- * 64 Bits Float version of Void and Cluster
+ * Remember to link blue-noise-utils.js
+ *
+ * 64 Bits Float version
+ *
  * https://github.com/901D3/blue-noise.js
  *
  * Copyright (c) 901D3
- * This code and the whole project is licensed with GPLv3 license
+ * This code is licensed with GPLv3 license
  */
 
 "use strict";
@@ -18,6 +20,312 @@ var blueNoiseFloat64 = (function () {
    */
 
   /**
+   * Generate blue noise with void and cluster method, initial binary array using Poisson Disk Sampling
+   *
+   * @param {int} width - Output array "width"
+   * @param {int} height - Output array "height"
+   *
+   * @param {number} phase1Sigma - Phase 1 sigma
+   * @param {number} phase2Sigma - Phase 2 sigma
+   * @param {number} phase3Sigma - Phase 3 sigma
+   *
+   * @param {number[]} phase1Kernel - Phase 1 blur kernel
+   * @param {number[]} phase2Kernel - Phase 2 blur kernel
+   * @param {number[]} phase3Kernel - Phase 3 blur kernel
+   *
+   * @param {normalized} candidateFillingRatio - The ratio between phase 2 and phase 3
+   * @param {binary[]} initArray - Initial binary array, must have the same dimension as width and height
+   *
+   * @returns {int[]} - Values ranging from 0 to (width * height)
+   */
+
+  function _voidAndCluster(
+    width,
+    height,
+    PDSRadiusX,
+    PDSRadiusY,
+    PDSKValue,
+    phase1Sigma,
+    phase2Sigma,
+    phase3Sigma,
+    phase1Kernel,
+    phase2Kernel,
+    phase3Kernel,
+    candidateFillingRatio,
+    initArray,
+    verbose
+  ) {
+    let t0;
+    let t1;
+    if (verbose) {
+      t0 = performance.now();
+      t1 = t0;
+    }
+
+    // Safety checks
+
+    if (width == null || height == null || PDSRadiusX == null || PDSRadiusY == null) {
+      throw new Error("'width', 'height', 'PDSRadiusX', 'PDSRadiusY', arguments is mandatory");
+    }
+
+    if (!Number.isInteger(width) || !Number.isInteger(height)) {
+      throw new Error("'width' and 'height' must be integers");
+    }
+
+    const phase1KernelCheck = phase1Kernel != null && Array.isArray(phase1Kernel);
+    const phase2KernelCheck = phase2Kernel != null && Array.isArray(phase2Kernel);
+    const phase3KernelCheck = phase3Kernel != null && Array.isArray(phase3Kernel);
+    if (!phase1Kernel) {
+      if (phase1Sigma == null) {
+        throw new Error("phase1Kernel is " + phase1Kernel + ". 'phase1Sigma' arguments is mandatory");
+      }
+    }
+
+    if (PDSKValue == null) {
+      console.warn("PDSKValue falled back to " + 30);
+      PDSKValue = 30;
+    }
+
+    if (phase2Sigma == null) {
+      console.warn("phase2Sigma falled back to " + phase1Sigma);
+      phase2Sigma = phase1Sigma;
+    }
+
+    if (phase3Sigma == null) {
+      console.warn("phase3Sigma falled back to " + phase1Sigma);
+      phase3Sigma = phase1Sigma;
+    }
+
+    if (candidateFillingRatio == null) {
+      console.warn("candidateFillingRatio falled back to " + 0.5);
+      candidateFillingRatio = 0.5;
+    }
+
+    // Get custom kernel dimension before flat them
+    let phase1KernelWidth;
+    let phase1KernelHeight;
+    let phase2KernelWidth;
+    let phase2KernelHeight;
+    let phase3KernelWidth;
+    let phase3KernelHeight;
+
+    if (phase1KernelCheck) {
+      phase1KernelHeight = phase1Kernel.length;
+      phase1KernelWidth = phase1Kernel[0].length;
+      phase1Kernel = new Float64Array(phase1Kernel.flat());
+    } else {
+      phase1KernelHeight = 2 * Math.ceil(3 * phase1Sigma) + 1;
+      phase1KernelWidth = phase1KernelHeight;
+    }
+
+    if (phase2KernelCheck) {
+      phase2KernelWidth = phase2Kernel.length;
+      phase2KernelHeight = phase2Kernel[0].length;
+      phase2Kernel = new Float64Array(phase2Kernel.flat());
+    } else {
+      phase2KernelHeight = 2 * Math.ceil(3 * phase2Sigma) + 1;
+      phase2KernelWidth = phase2KernelHeight;
+    }
+
+    if (phase3KernelCheck) {
+      phase3KernelWidth = phase3Kernel.length;
+      phase3KernelHeight = phase3Kernel[0].length;
+      phase3Kernel = new Float64Array(phase3Kernel.flat());
+    } else {
+      phase3KernelHeight = 2 * Math.ceil(3 * phase3Sigma) + 1;
+      phase3KernelWidth = phase3KernelHeight;
+    }
+
+    const sqSz = width * height;
+
+    // Setup arrays
+    const binArray = new Uint8Array(sqSz);
+    const rankArray = new Int32Array(sqSz);
+    const blurred = new Float64Array(sqSz);
+    const pds = blueNoiseUtils.poissonDiskSampling(width, height, PDSRadiusX, PDSRadiusY, PDSKValue);
+
+    if (initArray && initArray.flat().length === sqSz) {
+      binArray.set(initArray.flat());
+    } else {
+      console.warn(
+        "Inputed initial array dimension does not match " + width + "x" + height + ", default to Poisson Disk Sampling"
+      );
+
+      // Set binary array to PDS if user did not provide custom initial binary array
+      binArray.set(pds);
+    }
+
+    const filled1 = binArray.reduce((sum, v) => sum + v, 0);
+    candidateFillingRatio = filled1 + Math.floor(Math.min(Math.max(candidateFillingRatio, 0), 1) * (sqSz - filled1));
+
+    let kernel;
+    if (!phase1KernelCheck) {
+      kernel = _getGaussianKernelLUT(phase1Sigma);
+      phase1KernelHeight = 2 * Math.ceil(3 * phase1Sigma) + 1;
+      phase1KernelWidth = phase1KernelHeight;
+    }
+
+    if (verbose) {
+      console.info("Number of initial placed points: " + filled1);
+      console.info("Candidate filling ratio: " + candidateFillingRatio);
+      console.info("Setup took " + (performance.now() - t1) + "ms");
+
+      t1 = performance.now();
+    }
+
+    // Phase 1
+    // Temporary binary array, original binary array is left unchanged after phase 1
+    const temp = new Uint8Array(binArray);
+    blueNoiseUtils.blurWrap(
+      binArray,
+      width,
+      height,
+      phase1KernelCheck ? phase1Kernel : kernel,
+      blurred,
+      phase1KernelWidth,
+      phase1KernelHeight
+    );
+
+    for (let rank = 0; rank < filled1; rank++) {
+      let value = -Infinity;
+      let idx;
+
+      for (let j = 0; j < sqSz; j++) {
+        const blurredValue = blurred[j];
+        if (temp[j] === 1 && blurredValue > value) {
+          value = blurredValue;
+          idx = j;
+        }
+      }
+
+      // Remove "1" from tightest cluster in Binary Pattern.
+      temp[idx] = 0;
+      rankArray[idx] = filled1 - rank;
+
+      blueNoiseUtils.deltaBlurUpdate(
+        width,
+        height,
+        idx,
+        -1,
+        blurred,
+        phase1KernelCheck ? phase1Kernel : kernel,
+        phase1KernelWidth,
+        phase1KernelHeight
+      );
+    }
+    // End of Phase 1
+
+    if (verbose) {
+      console.info("Phase 1 took " + (performance.now() - t1) + "ms");
+
+      t1 = performance.now();
+    }
+
+    // Phase 2
+    if (!phase2KernelCheck) kernel = _getGaussianKernelLUT(phase2Sigma);
+
+    blueNoiseUtils.blurWrap(
+      binArray,
+      width,
+      height,
+      phase2KernelCheck ? phase2Kernel : kernel,
+      blurred,
+      phase2KernelWidth,
+      phase2KernelHeight
+    );
+
+    // Go from
+    for (let rank = filled1; rank < candidateFillingRatio; rank++) {
+      let value = Infinity;
+      let idx;
+
+      // Find location of tightest cluster in Binary Pattern.
+      for (let j = 0; j < sqSz; j++) {
+        const blurredValue = blurred[j];
+        if (binArray[j] === 0 && blurredValue < value) {
+          value = blurredValue;
+          idx = j;
+        }
+      }
+
+      // Remove "0" from largest void in Binary Pattern.
+      binArray[idx] = 1;
+      rankArray[idx] = rank;
+
+      blueNoiseUtils.deltaBlurUpdate(
+        width,
+        height,
+        idx,
+        1,
+        blurred,
+        phase2KernelCheck ? phase2Kernel : kernel,
+        phase2KernelWidth,
+        phase2KernelHeight
+      );
+    }
+    // End of Phase 2
+
+    if (verbose) {
+      console.info("Phase 2 took " + (performance.now() - t1) + "ms");
+
+      t1 = performance.now();
+    }
+
+    // Phase 3
+    if (!phase3KernelCheck) kernel = _getGaussianKernelLUT(phase3Sigma);
+
+    // Copy binary array to temp and invert it, 0 becomes 1 and vice versa
+    for (let i = 0; i < sqSz; i++) temp[i] = 1 ^ binArray[i];
+
+    // Blur the temp array, so we can use binArray[idx] === 0
+    blueNoiseUtils.blurWrap(
+      temp,
+      width,
+      height,
+      phase3KernelCheck ? phase3Kernel : kernel,
+      blurred,
+      phase3KernelWidth,
+      phase3KernelHeight
+    );
+
+    // Fills in the remaining "0s" in binArray so rankArray is complete blue noise without any voids
+    for (let rank = candidateFillingRatio; rank < sqSz; rank++) {
+      let value = -Infinity;
+      let idx;
+
+      for (let j = 0; j < sqSz; j++) {
+        const blurredValue = blurred[j];
+        if (binArray[j] === 0 && blurredValue > value) {
+          value = blurredValue;
+          idx = j;
+        }
+      }
+
+      binArray[idx] = 1;
+      rankArray[idx] = rank;
+      blueNoiseUtils.deltaBlurUpdate(
+        width,
+        height,
+        idx,
+        -1,
+        blurred,
+        phase3KernelCheck ? phase3Kernel : kernel,
+        phase3KernelWidth,
+        phase3KernelHeight
+      );
+    }
+    // End of Phase 3
+
+    if (verbose) {
+      console.info("Phase 3 took " + (performance.now() - t1) + "ms");
+      console.info("Total time: " + (performance.now() - t0) + "ms");
+    }
+
+    return rankArray;
+  }
+
+  /**
+   * (OLD)
    * Generate blue noise with void and cluster method, initial binary array using Poisson Disk Sampling
    *
    * @param {int} width - Output array "width"
@@ -34,14 +342,14 @@ var blueNoiseFloat64 = (function () {
    * @param {number[]} phase2Kernel - Phase 2 blur kernel
    * @param {number[]} phase3Kernel - Phase 3 blur kernel
    *
-   * @param {normalized} candidateFillingRatio - Phase 3 Gaussian kernel size capping
+   * @param {normalized} candidateFillingRatio - The ratio between phase 2 and phase 3
    * @param {Uint8Array} initArray - Initial array, it act like user inputted seed, must have the same length as (<width> * <height>), array dimension(2D 3D or 4D) doesn't matter
    * If no array if supplied it will default to randomized Poisson Disk Sampling
    *
    * @returns {array}
    */
 
-  function _voidAndCluster(
+  function _voidAndClusterGradient(
     width,
     height,
     PDSRadiusX,
@@ -357,9 +665,9 @@ var blueNoiseFloat64 = (function () {
 
     if (!gaussianKernelLUT.has(sigma)) {
       const kernelSize = 2 * radius + 1;
-      const kernel = new Float64Array(Math.pow(kernelSize, 2));
+      const kernel = new Float64Array(kernelSize * kernelSize);
       let sum = 0;
-      const denom = 2 * Math.pow(sigma, 2);
+      const denom = 2 * sigma * sigma;
 
       for (let y = -radius; y <= radius; y++) {
         const yOffs = (y + radius) * kernelSize;
@@ -381,8 +689,7 @@ var blueNoiseFloat64 = (function () {
 
   return {
     voidAndCluster: _voidAndCluster,
+    voidAndClusterGradient: _voidAndClusterGradient,
     getGaussianKernelLUT: _getGaussianKernelLUT,
   };
 })();
-
-
