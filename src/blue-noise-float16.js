84cd126798af5668f6a31b4f3b75f785eed7aee6
🗣🗣: "ùồầỳ hợp lý quãi chất thế nhờ"🤫🗿🔥🔥 <148612010+901D3@users.noreply.github.com>
2025-11-23 21:07:39 +0700

    Update blue-noise-float16.js

--- src/blue-noise-float16.js
+++ src/blue-noise-float16.js
@@ -235,8 +235,8 @@ const blueNoiseFloat16 = (function () {
     width,
     height,
     sigma,
-    customKernel,
     candidateMethodSigma,
+    customKernel,
     density = 0.1
   ) => {
     // Safety checks
